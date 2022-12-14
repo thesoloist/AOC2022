fef8934f3904f915304019c39ab1f3cf6423a90d
hxdai <huaxin.dai@outlook.com>
2022-12-14 16:00:15 -0500

    pt2 works for sample, but input too large for it...

--- Day14.py
+++ Day14.py
@@ -66,35 +66,48 @@ def print_map(m):
         print(l)
     print("\n")
 
-def drop(pt, state_map):
+def drop(pt, state_map, has_floor):
     my_state = []
     edge = None
     sand_x, sand_y = pt[0], pt[1]
+    if(state_map[sand_y][sand_x] == 'o'):
+        print("Gummed up, return...")
+        return (None, None)
     for l in state_map:
         my_state.append([])
         my_state[-1] = [c for c in l]
     if(sand_y+1 >= len(my_state)): #gonna fall out bottom
         return None, None    
     elif my_state[sand_y+1][sand_x] == '.': # down-middle
-        my_state, edge = drop((sand_x, sand_y+1), my_state)
+        my_state, edge = drop((sand_x, sand_y+1), my_state, has_floor)
     else:
         can_left = (sand_x!=0)
         if(can_left):
             can_left = my_state[sand_y+1][sand_x-1] == '.'
+        elif(has_floor):
+            # if we are at the edge AND on the floor, add it and return right here
+            if(sand_y == len(my_state)-2):
+                my_state[sand_y][sand_x] = 'o'
+                return (my_state, None)                    
+            else:
+                return (None, True)
+
         can_right = (sand_x != (len(my_state[sand_y+1])-1))
         if(can_right):
             can_right = my_state[sand_y+1][sand_x+1] == '.'        
+        elif(has_floor):
+            # if we are at the edge AND on the floor, add it and return right here
+            if(sand_y == len(my_state)-2):
+                my_state[sand_y][sand_x] = 'o'
+                return (my_state, None)                    
+            else:                
+                return (None, False)
 
         if can_left:            
-            my_state, edge = drop((sand_x-1, sand_y+1), my_state)
+            my_state, edge = drop((sand_x-1, sand_y+1), my_state, has_floor)
         elif can_right:
-            my_state, edge = drop((sand_x+1, sand_y+1), my_state)
-        else:
-            # can't go anywhere, see if it's edge
-            if(sand_x==0):
-                return (None, True)
-            elif(sand_x == (len(my_state[sand_y+1])-1)):
-                return (None, False)
+            my_state, edge = drop((sand_x+1, sand_y+1), my_state, has_floor)
+        else:            
             my_state[sand_y][sand_x] = 'o'
     return (my_state, edge)
 
@@ -115,8 +128,8 @@ def solve(drop_point, floor_expand):
                 updated_map.append(['.' for c in range(len(updated_map[-1]))])
         
     while True:
-        mdp = (drop_point[0] + horiz_expand, drop_point[1])
-        (cur_state, edge) = drop(drop_point, updated_map)        
+        mdp = (drop_point[0] + horiz_expand, drop_point[1])        
+        (cur_state, edge) = drop(mdp, updated_map, floor_expand>0)
         if(cur_state == None):
             # stablized or falling off the edge?
             if(edge == None):
@@ -127,15 +140,11 @@ def solve(drop_point, floor_expand):
                 # falling off edge. if floor_expand>0, horizontally expand map
                 if(floor_expand > 0):
                     print("Off current floor edge, expanding horizontally...")
-                    for i in range(len(updated_map)-2):
-                        updated_map[i] = ['.'] + updated_map[i] + ['.']
-                    if(edge == True):
-                        updated_map[-2] = ['o'] + updated_map[-2] + ['.']
-                    else:
-                        updated_map[-2] = ['.'] + updated_map[-2] + ['o']
+                    for i in range(len(updated_map)-1):
+                        updated_map[i] = ['.'] + updated_map[i] + ['.']                    
                     updated_map[-1] = ['#'] + updated_map[-1] + ['#']
                     horiz_expand += 1
-                    print_map(updated_map)
+                    #print_map(updated_map)
                 else:
                     print("off edge, but no floor, so consider stablized as well, rnd_cnt = ", rnd_cnt)
                     print_map(updated_map)
@@ -146,7 +155,7 @@ def solve(drop_point, floor_expand):
             for l in range(len(updated_map)):
                 for c in range(len(updated_map[l])):
                     updated_map[l][c] = cur_state[l][c] 
-            print_map(updated_map)
+            # print_map(updated_map)
             rnd_cnt +=1
             if(rnd_cnt % 100 == 0):
                 print("Done round ",rnd_cnt)
