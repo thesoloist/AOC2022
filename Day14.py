1ce06c777b1bcac2f4074b4e839f7ac6bf608a40
Huaxin Dai <huaxin.dai@outlook.com>
2022-12-14 20:27:28 -0500

    doing subtraction for pt2 is so much better than addition

--- Day14.py
+++ Day14.py
@@ -2,7 +2,7 @@ cave_map = {} # key=depth, value=list of columns at the depth
 
 cleared_cave_map_arr = []
 lo_col, hi_col = 9999999999,0
-with open("day14_sample.txt") as f:
+with open("day14_input.txt") as f:
     for l in f.readlines():
         nodes = l.rstrip().split(' -> ')
         start_node = nodes[0].split(',')
@@ -66,55 +66,57 @@ def print_map(m):
         print(l)
     print("\n")
 
-def drop(pt, state_map, has_floor):
-    my_state = []
-    edge = None
+def drop(pt, state_map):
+    my_state = []    
     sand_x, sand_y = pt[0], pt[1]
     if(state_map[sand_y][sand_x] == 'o'):
         print("Gummed up, return...")
-        return (None, None)
+        return None
     for l in state_map:
         my_state.append([])
         my_state[-1] = [c for c in l]
     if(sand_y+1 >= len(my_state)): #gonna fall out bottom
-        return None, None    
+        return None
     elif my_state[sand_y+1][sand_x] == '.': # down-middle
-        my_state, edge = drop((sand_x, sand_y+1), my_state, has_floor)
+        my_state = drop((sand_x, sand_y+1), my_state)
     else:
-        can_left = (sand_x!=0)
-        if(can_left):
-            can_left = my_state[sand_y+1][sand_x-1] == '.'
-        elif(has_floor):
-            # if we are at the edge AND on the floor, add it and return right here
-            if(sand_y == len(my_state)-2):
-                my_state[sand_y][sand_x] = 'o'
-                return (my_state, None)                    
-            else:
-                return (None, True)
-
-        can_right = (sand_x != (len(my_state[sand_y+1])-1))
-        if(can_right):
-            can_right = my_state[sand_y+1][sand_x+1] == '.'        
-        elif(has_floor):
-            # if we are at the edge AND on the floor, add it and return right here
-            if(sand_y == len(my_state)-2):
-                my_state[sand_y][sand_x] = 'o'
-                return (my_state, None)                    
-            else:                
-                return (None, False)
-
-        if can_left:            
-            my_state, edge = drop((sand_x-1, sand_y+1), my_state, has_floor)
-        elif can_right:
-            my_state, edge = drop((sand_x+1, sand_y+1), my_state, has_floor)
+        if(sand_x==0): #gonna fall out left
+            return None
+        elif(my_state[sand_y+1][sand_x-1] == '.'): # can fall left
+            my_state = drop((sand_x-1, sand_y+1), my_state)
+        elif (sand_x == (len(my_state[sand_y+1])-1)): #gonna fall out right
+            return None
+        elif (my_state[sand_y+1][sand_x+1] == '.'):
+            my_state = drop((sand_x+1, sand_y+1), my_state)
         else:            
             my_state[sand_y][sand_x] = 'o'
-    return (my_state, edge)
+    return my_state
 
-def solve(drop_point, floor_expand):
+def part1(drop_point):
     rnd_cnt = 0
     updated_map = []
-    horiz_expand = 0
+    for l in cleared_cave_map_arr:
+        updated_map.append([])
+        updated_map[-1] = [c for c in l]  
+        
+    while True:      
+        cur_state = drop(drop_point, updated_map)
+        if(cur_state == None):           
+            print("Stablized, rnd_cnt = ",rnd_cnt)
+            #print_map(updated_map)
+            break            
+        else:
+            for l in range(len(updated_map)):
+                for c in range(len(updated_map[l])):
+                    updated_map[l][c] = cur_state[l][c] 
+            #print_map(updated_map)
+            rnd_cnt +=1
+            if(rnd_cnt % 100 == 0):
+                print("Done round ",rnd_cnt)
+                #print_map(updated_map)
+   
+def part2(drop_point, floor_expand):    
+    updated_map = []
     for l in cleared_cave_map_arr:
         updated_map.append([])
         updated_map[-1] = [c for c in l]
@@ -126,44 +128,26 @@ def solve(drop_point, floor_expand):
                 updated_map.append(['#' for c in range(len(updated_map[-1]))])
             else:
                 updated_map.append(['.' for c in range(len(updated_map[-1]))])
-        
-    while True:
-        mdp = (drop_point[0] + horiz_expand, drop_point[1])        
-        (cur_state, edge) = drop(mdp, updated_map, floor_expand>0)
-        if(cur_state == None):
-            # stablized or falling off the edge?
-            if(edge == None):
-                print("Stablized, rnd_cnt = ",rnd_cnt)
-                print_map(updated_map)
-                break
-            else:
-                # falling off edge. if floor_expand>0, horizontally expand map
-                if(floor_expand > 0):
-                    print("Off current floor edge, expanding horizontally...")
-                    for i in range(len(updated_map)-1):
-                        updated_map[i] = ['.'] + updated_map[i] + ['.']                    
-                    updated_map[-1] = ['#'] + updated_map[-1] + ['#']
-                    horiz_expand += 1
-                    #print_map(updated_map)
-                else:
-                    print("off edge, but no floor, so consider stablized as well, rnd_cnt = ", rnd_cnt)
-                    print_map(updated_map)
-                    break
-                #if left edge, deposit at map[-2][0]
-                #else deposit at map[-2][-1]
-        else:
-            for l in range(len(updated_map)):
-                for c in range(len(updated_map[l])):
-                    updated_map[l][c] = cur_state[l][c] 
-            # print_map(updated_map)
-            rnd_cnt +=1
-            if(rnd_cnt % 100 == 0):
-                print("Done round ",rnd_cnt)
-                #print_map(updated_map)
-    #if falling off edge, return none
-    #return None
-    
-#solve(drop_point, 0)
+
+    # process updated_map:        
+    total_dots = len(updated_map) - 1
+    for i in range(len(updated_map) - 1):
+        total_dots += 2*i
+    print("Total possible dots = ", total_dots)
+    # remove existing rocks
+    for idy, l in enumerate(updated_map[:-floor_expand+1]):
+        for idx, c in enumerate(l):
+            if c=='#':
+                #print("Found rock")
+                total_dots -= 1
+            elif(0<idy and 0<idx<len(l)-1):
+                # remove points with a l-c-r ceiling
+                if updated_map[idy-1][idx-1] == '#' and updated_map[idy-1][idx]=='#' and updated_map[idy-1][idx+1] =='#':
+                    #print("Found point with full ceiling")
+                    total_dots -=1
+                    #since it's not possible, might as well consider it as rock. should be in time for next row
+                    updated_map[idy][idx] = "#"
+    print("After rock/ceiling, total possible dots = ", total_dots)
+#part1(drop_point)
 expand = 2
-#drop_point = (drop_point[0] + expand, drop_point[1])
-solve(drop_point, expand)
+part2(drop_point, expand)
